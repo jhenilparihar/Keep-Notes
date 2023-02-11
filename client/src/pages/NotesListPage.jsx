d75e7b718c0a3466774895791a31be3d561c6748
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-11 16:19:57 +0530

    render notes data

--- client/src/pages/NotesListPage.jsx
+++ client/src/pages/NotesListPage.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
+import ListItem from "../components/ListItem";
 
 const NotesListPage = () => {
     let [notes, setNotes] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         getNotes();
-    }, [] )
+    }, []);
 
     let getNotes = async () => {
-        let response = await fetch("http://127.0.0.1:8000/api/notes/")
-        let data = await response.json()
-        console.log('DATA: ', data)
-        setNotes(data)
-    } 
-    return <div>Notes </div>;
+        let response = await fetch("http://127.0.0.1:8000/api/notes/");
+        let data = await response.json();
+        console.log("DATA: ", data);
+        setNotes(data);
+    };
+    return (
+        <div>
+            <div className="notes-list">
+                {notes.map((note, index) => (
+                    <ListItem key={index} note={note}/>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default NotesListPage;
