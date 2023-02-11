32454a66841895b45b4d7293800d4c4ee8fcfded
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-12 00:03:09 +0530

    added toggle view mode feature

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const Header = () => {
+  let [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    setIsDark(document.querySelector(".container") !== null);
+  }, []);
+
+  let changeMode = () => {
+    const div = document.querySelector(".container");
+    div.classList.toggle("dark");
+    setIsDark(!isDark);
+  };
+  console.log(document.querySelector(".container"));
   return (
     <div>
       <div className="app-header">
         <h1>Note List</h1>
+        {isDark ? (
+          <span className="mode-icon" onClick={changeMode}>
+            Light Mode
+          </span>
+        ) : (
+          <span className="mode-icon" onClick={changeMode}>
+            Dark Mode
+          </span>
+        )}
       </div>
 
       <Outlet />
