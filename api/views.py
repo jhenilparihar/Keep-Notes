767ad50bead2cbe2bb08053f1989e35aa48ed82a
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-11 14:20:22 +0530

    adding django REST framework

--- api/views.py
+++ api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
+
+@api_view(['GET'])
 def getRoutes(request):
+
     routes = [
         {
             'Endpoint': '/notes/',
@@ -36,4 +42,19 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
